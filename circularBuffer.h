cf74e16817bc5b007e698f6d6c832065c06261b4
unknown <rssimon@alaska.edu>
2014-01-10 13:33:43 -0500

    removing .cpp file since it isn't even necessary.

--- circularBuffer.h
+++ circularBuffer.h
@@ -35,7 +35,10 @@ THE SOFTWARE.
 #define CIRCULAR_BUFFER_H_
 
 #include <iostream>
+#include <locale>
 #include <deque>
+#include <stdlib.h>
+#include <string>
 
 template <typename T>
 
@@ -54,5 +57,113 @@ class CircularBuffer
 		std::deque<T> _cBuffer;
 		int posLastElement;
 };
+
+//Constructor with size of the circular buffer as the parameter. Definition.
+
+template <typename T>
+CircularBuffer<T>::CircularBuffer(std::size_t size)
+{
+	for (int i = 0; i<size; i++)
+	{
+		_cBuffer.push_back("");
+	}
+}
+
+//Function to list all elements of the circular buffer. Definition.
+
+template <typename T>
+void CircularBuffer<T>::listCircularBuffer()
+{
+	for (int i = 0; i<_cBuffer.size(); i++)
+	{
+		std::cout<<_cBuffer[i]<<"\n";
+	}
+}
+
+//Function to append elements to the end of the circular buffer. Definition.
+
+template <typename T>
+void CircularBuffer<T>::addCircularBuffer()
+{
+	
+}
+
+bool verifyFirstInput(std::string input)
+{
+	int cbSize = atoi(input.c_str());
+	if (cbSize >= 0 && cbSize <= 10000)
+	{
+		return true;
+	}	
+	else
+	{
+		return false;
+	}	
+}
+
+template <typename T>
+int appendBuffer(CircularBuffer<T> & buffer, std::string input)
+{
+	for (int i = 0; i<atoi((input.substr(1)).c_str()); i++)
+	{
+		std::cout<<"hi\n";
+	}
+}
+
+template <typename T>
+int removeBuffer(CircularBuffer<T> & buffer, std::string input)
+{
+
+}
+
+int main()
+{
+
+//do...while to get input from the use to manipulate the circular buffer.
+
+bool control = false;  //Control to ensure that the first input is a number between 0 and 10000, to be set as buffer size.
+std::string inputContainer;  //Contains the input from the user;
+do
+{
+	
+	std::cin>>inputContainer;
+		if (control == false)
+		{
+			control = verifyFirstInput(inputContainer);
+			std::cout<<"Enter a valid size of the container first.\n";
+		}
+		if (inputContainer == "Q")
+			break;
+		else
+		{ 
+			CircularBuffer<std::string> buffer(atoi(inputContainer.c_str()));
+		
+			do
+			{
+				std::cin>>inputContainer;
+			
+				switch(inputContainer[0])
+				{
+					case 'A':
+								appendBuffer(buffer, inputContainer);
+								break;
+					case 'R':
+								removeBuffer(buffer, inputContainer);
+								break;
+					case 'L':
+								buffer.listCircularBuffer();
+								break;
+					case 'Q':
+								break;
+					default:
+								std::cout<<"I cannot understand what you are saying.";
+				}
+			
+			}while(inputContainer!="Q");
+		}	
+	
+}while(inputContainer!="Q");
+
+}
 
 #endif
